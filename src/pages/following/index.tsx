5cf4a4ea1700e6ccc5bd1ad85045b8c4997b59f1
YIXUN TSENG <scps960740@gmail.com>
2022-02-24 20:21:12 +0800

    init
    Change-Id: Ibfb226c6b23ac844610d1787d321e678917f34ad

---
+++ src/pages/following/index.tsx
@@ -0,0 +1,5 @@
+const Following: React.FC = () => {
+  return <p>Following</p>;
+};
+
+export default Following;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,5 @@
+const Home: React.FC = () => {
+  return <p>Home</p>;
+};
+
+export default Home;
